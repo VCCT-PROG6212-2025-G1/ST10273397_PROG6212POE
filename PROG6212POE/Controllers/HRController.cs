b6a658c9be0146f9a6b1e3445981136378be0562
Nicholas Liam Wolfaardt <ST10273397@vcconnect.edu.za>
2025-11-21 12:05:53 +0200

    HR Can Now Create Users

--- PROG6212POE/Controllers/HRController.cs
+++ PROG6212POE/Controllers/HRController.cs
@@ -1,20 +1,55 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using PROG6212POE.Data;
 using PROG6212POE.Models;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
 
 namespace PROG6212POE.Controllers
 {
     public class HRController : Controller
     {
+
+        private readonly AppDbContext _context;
+
+        //Constructor injects the database context
+        public HRController(AppDbContext context)
+        {
+            _context = context;
+        }
+
         [HttpGet]
         public IActionResult Index()
         {
-            return View();
+            var users = _context.UserModel.ToList();
+            return View(users);
         }
 
         [HttpGet]
         public IActionResult Edit()
         {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(UserModel user)
+        {
+
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+
+            if (user == null)
+            {
+                // Add a general validation error
+                ModelState.AddModelError("", "Please Fill in All Details");
+                return View(); // Return the login view with the error displayed
+            }
+
+            _context.UserModel.Update(user);
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction("Index");
         }
 
         [HttpGet]
@@ -26,8 +61,29 @@ namespace PROG6212POE.Controllers
         [HttpGet]
         public IActionResult CreateUser()
         {
-            return View();
+            return View(new UserModel());
         }
 
+        [HttpPost]
+        public async Task<IActionResult> CreateUser(UserModel user)
+        {
+
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+
+            if (user == null)
+            {
+                // Add a general validation error
+                ModelState.AddModelError("", "Please Fill in All Details");
+                return View(); // Return the login view with the error displayed
+            }
+
+            _context.UserModel.Add(user);
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction("Index");
+        }
     }
 }
